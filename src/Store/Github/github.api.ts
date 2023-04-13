7e95837e6135f31af7b3ac6d3ee966d663bf08f6
Athony A <astremsky@gmail.com>
2023-04-12 23:49:02 -0400

    api file with endpoints

---
+++ src/Store/Github/github.api.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+export const githubApi = createApi({
+  reducerPath: 'github/api',
+  baseQuery: fetchBaseQuery({
+    baseUrl:'https://api/github.com/'
+  }),
+  endpoints: build => ({
+    searchUsers: build.query({
+      query: () => 'search/users'
+    })
+  })
+})
--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import { store } from "./Store";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -20,7 +21,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
